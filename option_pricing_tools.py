6991693b6c19b92fe5b8c0bbe1a781e0f1fa9dd2
echudov <echudov@gmail.com>
2020-09-13 14:27:57 -0500

    implemented binomial, but not tested yet

--- option_pricing_tools.py
+++ option_pricing_tools.py
@@ -1,5 +1,6 @@
 import math
 from scipy.stats import norm
+import numpy as np
 import decimal
 
 
@@ -13,9 +14,29 @@ def black_scholes(strike: float, t: float, r: float, volatility: float, spot_pri
         return cum(-d2) * strike * math.e ** (-r * t) - cum(-d1) * spot_price
 
 
-# not implemented
-def binomial(strike, t, r, volatility, spot_price, option_type='call'):
-    return 0
+# implemented but not tested yet
+def binomial(strike, T, r, volatility, spot_price, N=6, option_type='call'):
+    values = np.zeros((N+1, (2 * N) + 1)) # option values at iteration (n, element)
+    prices = np.zeros((N+1, (2 * N) + 1)) # equity prices to evaluate the option at
+
+    dt = T / N
+    u = math.exp(volatility * math.sqrt(dt))
+    d = 1 / u
+    p = (math.exp(r * dt) - d)/(u - d)
+    q = 1 - p
+    for i in range(N + 1):
+        for j in range(i + 1):
+            prices[j, i] = spot_price * (u ** (i - j)) * (d ** j)
+
+    if option_type == 'call':
+        values[:, N] = np.maximum(np.zeros(N + 1), (prices[:, N] - strike))
+    else:
+        values[:, N] = np.maximum(np.zeros(N + 1), (strike - prices[:, N]))
+    for i in reversed(range(N - 1)):
+        for j in range(0, i + 1):
+            values[j, i] = 1 / (1 + r) * (p * values[j, i + 1] + q * values[j + 1, i + 1])
+
+    return values[0, 0]
 
 # should try to implement this and the above just for shits and giggles
 # def monte_carlo(params)
